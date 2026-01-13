68b8cf52fadc0be85e3704939ace16a23fd11fc0
Leart Bajrami <73801402+Leart-C@users.noreply.github.com>
2026-01-13 14:05:11 +0100

    Created Edit blade

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [ChirpController::class, 'index']);
-Route::post('/chirps', [ChirpController::class, 'store']);
+Route::resource('chirps', ChirpController::class)->only(['store', 'edit', 'update', 'destroy', 'index']);
